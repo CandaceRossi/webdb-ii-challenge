eff604cc68295791f8594e8fa8f02c0a680ea584
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-11-05 19:00:11 -0500

    built knex migration and database, checked sqlite, met MVP

---
+++ data/migrations/20191105165846_cars.js
@@ -0,0 +1,14 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("cars", function(table) {
+    table.increments();
+    table.varchar("VIN", 128).notNullable();
+    table.string("make", 64).notNullable();
+    table.string("model", 64).notNullable();
+    table.float("mileage");
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("cars");
+};
